7cdfa34abdbfab28236d4f761c62aebeb9f71beb
Ramana Kumar <ramana@member.fsf.org>
2024-07-29 16:53:24 +0100

    Add rocketlend abi and test reading the contract

--- src/pages/explorer.tsx
+++ src/pages/explorer.tsx
@@ -1,15 +1,18 @@
 import type { NextPage } from 'next';
-import { useChainId } from 'wagmi';
-import { constants, chainNameFromId } from '../wagmi';
+import { useChainId, useReadContract } from 'wagmi';
+import { constants, chainNameFromId, abi } from '../wagmi';
 
 const Page: NextPage = () => {
   const chainId = useChainId();
   const chainName = chainNameFromId(chainId);
   const contractEnvKey: string = `${chainName.toUpperCase()}_ROCKETLEND`;
-  const addr = constants[contractEnvKey];
+  const address = constants[contractEnvKey];
+  const testRead = useReadContract({abi, address, functionName: 'rocketStorage'}).data;
   return (
     <div>
-    <p>Got chain {chainName} and RocketLend contract address via {contractEnvKey} '{addr}'</p>
+    <p>Got chain {chainName} and RocketLend contract address via {contractEnvKey} '{address}'</p>
+    <p>ABI has {abi.length.toString()} items</p>
+    <p>Got {testRead} from reading rocketlend.rocketStorage().</p>
     </div>
   )
 }
--- src/wagmi.ts
+++ src/wagmi.ts
@@ -4,17 +4,21 @@ import {
   mainnet,
   holesky,
 } from 'wagmi/chains';
+import abi from './rocketlend.abi';
 
 export const constants = {
   HOLESKY_RPC: 'http://localhost:56580',
   HOLESKY_ROCKETLEND: '0xbe18A1B61ceaF59aEB6A9bC81AB4FB87D56Ba167',
+  WALLETCONNECT_ID: '72d96e7ff3f184256a7677239be021a6',
 }
 
 export const chainNameFromId = (id) => id === mainnet.id ? 'mainnet' : 'holesky';
 
+export { abi };
+
 export const config = getDefaultConfig({
   appName: 'RocketLend',
-  projectId: '72d96e7ff3f184256a7677239be021a6',
+  projectId: constants['WALLETCONNECT_ID'],
   chains: [
     mainnet,
     holesky,
