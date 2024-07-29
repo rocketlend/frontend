b7cf73748a04a3ac5a14e8f9fe3cb50eeb3c50ac
Ramana Kumar <ramana@member.fsf.org>
2024-07-29 08:23:42 +0100

    Select chains

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,23 +1,15 @@
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
 import {
-  arbitrum,
-  base,
   mainnet,
-  optimism,
-  polygon,
-  sepolia,
+  holesky,
 } from 'wagmi/chains';
 
 export const config = getDefaultConfig({
-  appName: 'RainbowKit App',
-  projectId: 'YOUR_PROJECT_ID',
+  appName: 'RocketLend',
+  projectId: 'ROCKETLEND_PROJECT_ID',
   chains: [
     mainnet,
-    polygon,
-    optimism,
-    arbitrum,
-    base,
-    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [sepolia] : []),
+    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [holesky] : []),
   ],
   ssr: true,
 });
