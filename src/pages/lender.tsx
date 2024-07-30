8f3304e30d84de457655615de7255356e3cf76b6
Ramana Kumar <ramana@member.fsf.org>
2024-07-30 16:24:59 +0100

    Start pulling pieces out as separate components

--- src/pages/lender.tsx
+++ src/pages/lender.tsx
@@ -1,36 +1,51 @@
 import type { NextPage } from 'next';
 import { useChainId, useReadContract, useAccount } from 'wagmi';
 import { formatEther } from 'viem';
-import { chainNameFromId, useRocketAddress, rplABI } from '../wagmi';
+import { chainNameFromId, useRocketAddress, rocketLendABI, rplABI } from '../wagmi';
 
-const Page: NextPage = ({constants}) => {
-  const chainId = useChainId();
-  const chainName = chainNameFromId(chainId);
+const useIsRegisteredLender = ({address}) => {
+  return false; // TODO: fetch from the log server
+};
+
+const RPLBalance = () => {
+  const chainName = chainNameFromId(useChainId());
   const {data: rplAddress} = useRocketAddress({chainName, contractName: 'rocketTokenRPL'});
-  const {address: connectedAccount, status} = useAccount();
+  const {address: accountAddress, status: accountStatus} = useAccount();
   const {data: rplBalance, error: rplBalanceError, fetchStatus} = useReadContract({
     abi: rplABI,
     address: rplAddress,
     functionName: 'balanceOf',
-    args: [connectedAccount]
+    args: [accountAddress]
   });
   return (
-    <>
+    <section>
+      <h2>Your RPL Balance</h2>
+      <p>Connected account: {accountAddress} (status: {accountStatus})</p>
+      {accountStatus === 'connected' ?
+        <p>{typeof rplBalance == 'bigint' && formatEther(rplBalance)} RPL (error: {rplBalanceError?.message || 'no error'}, status: {fetchStatus})</p> :
+        <p>Not fetching RPL balance because connection status is {accountStatus}</p>
+      }
+    </section>
+  );
+};
+
+const CreateLendingPoolForm = () => {
+  return (
       <section>
-        <h2>Your RPL Balance</h2>
-        <p>Connected account: {connectedAccount} (status: {status})</p>
-        {status === 'connected' ?
-         <p>{typeof rplBalance == 'bigint' && formatEther(rplBalance)} RPL (error: {rplBalanceError?.message || 'no error'}, status: {fetchStatus})</p> :
-         <p>Not fetching RPL balance because connection status is {status}</p>
-        }
+        <h2>Create Lending Pool</h2>
       </section>
+  );
+}
+
+const Page: NextPage = ({constants}) => {
+  return (
+    <>
+      <RPLBalance />
       <section>
         <h2>Your Lending Pools</h2>
         <p>TODO only show this if there are existing pools for this lender</p>
       </section>
-      <section>
-        <h2>Create Lending Pool</h2>
-      </section>
+      <CreateLendingPoolForm />
     </>
   );
 };
