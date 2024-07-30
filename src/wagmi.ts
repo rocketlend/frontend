dd095165b9a10840a9554ff0ad3a5e58688dcb1b
Ramana Kumar <ramana@member.fsf.org>
2024-07-30 16:23:56 +0100

    Move constants into environment files

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -11,15 +11,15 @@ import rocketStorageABI from './rocketStorage.abi';
 
 export const constants = {
   holesky: {
-    rpc: 'http://localhost:55801',
-    logserver: 'http://localhost:8879',
-    rocketlend: '0x01cf58e264d7578D4C67022c58A24CbC4C4a304E',
-    rocketStorage: '0x594Fb75D3dc2DFa0150Ad03F99F97817747dd4E1',
+    rpc: process.env.NEXT_PUBLIC_HOLESKY_RPC,
+    logserver: process.env.NEXT_PUBLIC_HOLESKY_LOGSERVER,
+    rocketlend: process.env.NEXT_PUBLIC_HOLESKY_ROCKETLEND,
+    rocketStorage: process.env.NEXT_PUBLIC_HOLESKY_ROCKET_STORAGE,
   },
   mainnet: {
-    rocketStorage: '0x1d8f8f00cfa6758d7bE78336684788Fb0ee0Fa46',
+    rocketStorage: process.env.NEXT_PUBLIC_MAINNET_ROCKET_STORAGE,
   },
-  walletConnectId: '72d96e7ff3f184256a7677239be021a6',
+  walletConnectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_ID,
 };
 
 export const chainNameFromId = (id) => id === mainnet.id ? 'mainnet' : 'holesky';
