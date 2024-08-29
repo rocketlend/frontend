f6ee6a642a5967205adb7918cfa9148d35ce57a5
Bethany Pietroniro <bethany.pietroniro28@gmail.com>
2024-08-29 09:01:09 -0400

    some font options

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,30 +7,58 @@ import { WagmiProvider } from "wagmi";
 import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import Head from "next/head";
 import Layout from "../components/layout";
+import { Radio_Canada, Fredoka, Murecho, Ysabeau_Infant, Karla, Work_Sans } from "next/font/google";
 
 import { config } from "../wagmi";
 
 const client = new QueryClient();
 
+const workSans = Work_Sans({
+  subsets: ["latin"],
+});
+
+const karla = Karla({
+  subsets: ["latin"],
+})
+const ysabeau = Ysabeau_Infant({
+  subsets: ["latin"],
+})
+const murecho = Murecho({
+  subsets: ["latin"],
+})
+const fredoka = Fredoka({
+  subsets: ["latin"],
+})
+const radioCanada = Radio_Canada({
+  subsets: ["latin"],
+})
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <WagmiProvider config={config}>
-      <QueryClientProvider client={client}>
-        <RainbowKitProvider>
-          <Layout>
-            <Head>
-              <title>Rocket Lend</title>
-              <meta
-                content="Frontend for Rocket Lend: lending protocol for staked RPL"
-                name="description"
-              />
-              <link href="/favicon.ico" rel="icon" />
-            </Head>
-            <Component {...pageProps} />
-          </Layout>
-        </RainbowKitProvider>
-      </QueryClientProvider>
-    </WagmiProvider>
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${radioCanada.style.fontFamily};
+        }
+      `}</style>
+      <WagmiProvider config={config}>
+        <QueryClientProvider client={client}>
+          <RainbowKitProvider>
+            <Layout>
+              <Head>
+                <title>Rocket Lend</title>
+                <meta
+                  content="Frontend for Rocket Lend: lending protocol for staked RPL"
+                  name="description"
+                />
+                <link href="/favicon.ico" rel="icon" />
+              </Head>
+              <Component {...pageProps} />
+            </Layout>
+          </RainbowKitProvider>
+        </QueryClientProvider>
+      </WagmiProvider>
+    </>
   );
 }
 
