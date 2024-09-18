fd1c790083e1600754ddbf64e0ea13e8cb05e714
Ramana Kumar <ramana@member.fsf.org>
2024-09-18 22:19:12 +0100

    Add more pool info to display

--- src/pages/pool/[poolId]/index.tsx
+++ src/pages/pool/[poolId]/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from "next";
+import { formatEther } from "viem";
 import { useParams } from "next/navigation";
 import { useAccount, useReadContracts } from "wagmi";
 import { useRocketLendAddress } from "../../../hooks/useRocketLendAddress";
@@ -25,9 +26,12 @@ const Page: NextPage = () => {
   return (params && state ? (<section>
     <h2>Lending Pool {poolId.toString()}</h2>
     <ul>
-    <li>Interest Rate: {params.interestRate}%</li>
-    <li>(DEBUG) End Time in seconds: {params.endTime.toString()}</li>
+    <li>Interest Rate: {params.interestRate}%</li>{/*TODO: show effective doubled rate when overdue*/}
     <li>End Time: {DateTime.fromSeconds(Number(params.endTime)).toLocaleString(DateTime.DATETIME_FULL)}</li>
+    <li>Available: {formatEther(state.available)} RPL</li>
+    <li>Borrowed: {formatEther(state.borrowed)} RPL</li>
+    {!!state.reclaimed && <li>Reclaimed: {formatEther(state.reclaimed)} ETH</li>}
+    {!!state.allowance && <li>Debt Transfer Allowance: {formatEther(state.allowance)} RPL</li>}
     </ul>
   </section>) : error ? <p>Error: {error.message}</p> : <p>fetching data... status: {status}</p>);
 };
