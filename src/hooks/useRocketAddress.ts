eb1b889e40e98981ed0a89ed82abaf59532cbde2
Ramana Kumar <ramana@member.fsf.org>
2024-09-18 08:18:56 +0100

    Support rocketStorage in useRocketAddress

--- src/hooks/useRocketAddress.ts
+++ src/hooks/useRocketAddress.ts
@@ -11,7 +11,12 @@ export const useRocketAddress = (contractName: string) => {
     [mainnet.id]: '0x1d8f8f00cfa6758d7bE78336684788Fb0ee0Fa46',
     [holesky.id]: '0x594Fb75D3dc2DFa0150Ad03F99F97817747dd4E1',
   }[useChainId()] as `0x${string}`;
-  return useReadContract({
+  if (contractName == 'rocketStorage') return {
+    data: address,
+    error: null,
+    fetchStatus: 'idle',
+  };
+  else return useReadContract({
    address, abi,
    functionName: 'getAddress',
    args: [keccak256(stringToBytes(`contract.address${contractName}`))]
