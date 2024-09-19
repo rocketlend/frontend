6d1817957208445506ebe5656f240614adfbbc98
Ramana Kumar <ramana@member.fsf.org>
2024-09-19 12:51:06 +0100

    Make it a supply or withdraw form
    Also show RPL balance and allowance on pool page

--- src/pages/pool/[poolId]/index.tsx
+++ src/pages/pool/[poolId]/index.tsx
@@ -9,6 +9,14 @@ import { DateTime } from "luxon";
 import { TransactionSubmitter } from "../../../components/TransactionSubmitter";
 import { Switch } from "../../../components/switch";
 import { Input } from "../../../components/input";
+import { Field, Label } from "../../../components/fieldset";
+import { RPLAllowance } from "../../../components/rpl-allowance";
+import { RPLBalance } from "../../../components/rpl-balance";
+
+const safeParseEther = (s: string) => {
+  try { return parseEther(s); }
+  catch { return BigInt(0); }
+};
 
 const Page: NextPage = () => {
   const { address } = useAccount();
@@ -16,7 +24,7 @@ const Page: NextPage = () => {
   const poolId: bigint = BigInt(poolIdParam);
   const rocketLendAddress = useRocketLendAddress();
   const [supplyOpen, setSupplyOpen] = useState<boolean>(false);
-  const [supplyAmount, setSupplyAmount] = useState<string>("");
+  const [targetAmount, setTargetAmount] = useState<string>("");
   const rocketLendBase = {
     address: rocketLendAddress,
     abi: rocketLendABI,
@@ -28,22 +36,34 @@ const Page: NextPage = () => {
     ]
   });
   const { result: params } = results?.[0] || {};
-  const { result: state } = results?.[1] || {};
-  return (params && state ? (<section>
+  const { result: state } = results?.[1] || { available: BigInt(0) };
+  const available = state?.available || BigInt(0);
+  const [changeVerb, changePreposition, changeAmount] = safeParseEther(targetAmount) < available ?
+    ['Withdraw', 'from', formatEther(available - safeParseEther(targetAmount))] :
+    ['Supply', 'to', formatEther(safeParseEther(targetAmount) - available)];
+  return (params && state ? (<>
+    <section>
+    <RPLBalance />
+    <RPLAllowance />
+    </section>
+    <section>
     <h2>Lending Pool {poolId.toString()}</h2>
     <ul>
     <li>Interest Rate: {params.interestRate}%</li>{/*TODO: show effective doubled rate when overdue*/}
     <li>End Time: {DateTime.fromSeconds(Number(params.endTime)).toLocaleString(DateTime.DATETIME_FULL)}</li>
-    <li>Available: {formatEther(state.available)} RPL
+    <li>Available: {formatEther(available)} RPL
     <Switch checked={supplyOpen} onChange={setSupplyOpen} />
     {supplyOpen && (<>
-      <Input value={supplyAmount} onChange={(e) => setSupplyAmount(e.target.value)} />
+      <Field>
+        <Label>New Amount</Label>
+        <Input value={targetAmount} onChange={(e) => setTargetAmount(e.target.value)} />
+      </Field>
       <TransactionSubmitter
-        buttonText={`Supply ${supplyAmount} RPL to this pool`}
+        buttonText={`${changeVerb} ${changeAmount} RPL ${changePreposition} this pool`}
         address={rocketLendAddress}
         abi={rocketLendABI}
         functionName="changePoolRPL"
-        args={[poolId, state.available + parseEther(supplyAmount)]}
+        args={[poolId, safeParseEther(targetAmount)]}
         onSuccess={(receipt) => refetch({})} />
       </>)}
     </li>
@@ -51,7 +71,8 @@ const Page: NextPage = () => {
     {!!state.reclaimed && <li>Reclaimed: {formatEther(state.reclaimed)} ETH</li>}
     {!!state.allowance && <li>Debt Transfer Allowance: {formatEther(state.allowance)} RPL</li>}
     </ul>
-  </section>) : error ? <p>Error: {error.message}</p> : <p>fetching data... status: {status}</p>);
+    </section>
+  </>) : error ? <p>Error: {error.message}</p> : <p>fetching data... status: {status}</p>);
 };
 
 export default Page;
